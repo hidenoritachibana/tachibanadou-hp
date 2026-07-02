5f39f3c082401b09f2de53c960fd468d82e99406
WIN_hidenori.tachibana <hidenoritachibana01@gmail.com>
2026-07-02 11:40:12 +0900

    fix：未使用変数削除３

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,39 +1,5 @@
-import { FileText, ClipboardList, Receipt } from 'lucide-react';
 
 const DashboardPage = () => {
-
-  // 仮の JSON データ
-  const jsonData = {
-    quoteEnabled: true,
-    invoiceEnabled: true,
-    receiptEnabled: false,
-        profileEnabled: true,
-
-  };
-
-  const actions = [
-    {
-      title: '見積書作成',
-      description: '見積書を作成します',
-      icon: <ClipboardList className="w-8 h-8 text-green-600" />,
-      link: '/quote/create',
-      disabled: !jsonData.quoteEnabled,
-    },
-    {
-      title: '請求書作成',
-      description: '請求書を作成します',
-      icon: <FileText className="w-8 h-8 text-blue-600" />,
-      link: '/invoice/create',
-      disabled: !jsonData.invoiceEnabled,
-    },
-    {
-      title: '領収書作成',
-      description: '機能追加予定',
-      icon: <Receipt className="w-8 h-8 text-yellow-600" />,
-      link: '/receipt/create',
-      disabled: !jsonData.receiptEnabled,
-    },
-  ];
 
   return (
 
